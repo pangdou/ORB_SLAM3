68c8fb2dfc3d558c6cd9695db4f25a35198c1bab
wanrui <haowanrui@163.com>
2022-07-20 22:24:19 +0800

    comment System.h

--- include/System.h
+++ include/System.h
@@ -44,7 +44,7 @@
 namespace ORB_SLAM3
 {
 
-class Verbose
+class Verbose           // 控制输出log的级别，级别越高，log越详细。
 {
 public:
     enum eLevel
@@ -56,35 +56,35 @@ public:
         VERBOSITY_DEBUG=4
     };
 
-    static eLevel th;
+    static eLevel th;   // 设置的log级别
 
 public:
-    static void PrintMess(std::string str, eLevel lev)
+    static void PrintMess(std::string str, eLevel lev) // 当前log级别，小于等于设置级别就可以输出。
     {
         if(lev <= th)
             cout << str << endl;
     }
 
-    static void SetTh(eLevel _th)
+    static void SetTh(eLevel _th) // 设置log级别
     {
         th = _th;
     }
 };
 
-class Viewer;
-class FrameDrawer;
-class MapDrawer;
-class Atlas;
-class Tracking;
-class LocalMapping;
-class LoopClosing;
-class Settings;
+class Viewer;           //
+class FrameDrawer;      //
+class MapDrawer;        //
+class Atlas;            //
+class Tracking;         //
+class LocalMapping;     //
+class LoopClosing;      //
+class Settings;         //
 
-class System
+class System            // ORB-SLAM对外的接口类
 {
 public:
     // Input sensor
-    enum eSensor{
+    enum eSensor{       // 传感器类型
         MONOCULAR=0,
         STEREO=1,
         RGBD=2,
@@ -102,32 +102,39 @@ public:
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     // Initialize the SLAM system. It launches the Local Mapping, Loop Closing and Viewer threads.
+    // 初始化SLAM系统。发射LocalMapping，LoopClosing，Viewer线程。
     System(const string &strVocFile, const string &strSettingsFile, const eSensor sensor, const bool bUseViewer = true, const int initFr = 0, const string &strSequence = std::string());
 
     // Proccess the given stereo frame. Images must be synchronized and rectified.
+    // 处理双目图像。左右图必须同步且校正。
     // Input images: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.
     // Returns the camera pose (empty if tracking fails).
     Sophus::SE3f TrackStereo(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timestamp, const vector<IMU::Point>& vImuMeas = vector<IMU::Point>(), string filename="");
 
     // Process the given rgbd frame. Depthmap must be registered to the RGB frame.
+    // 深度图必须要与rgb图配准
     // Input image: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.
     // Input depthmap: Float (CV_32F).
     // Returns the camera pose (empty if tracking fails).
     Sophus::SE3f TrackRGBD(const cv::Mat &im, const cv::Mat &depthmap, const double &timestamp, const vector<IMU::Point>& vImuMeas = vector<IMU::Point>(), string filename="");
 
     // Proccess the given monocular frame and optionally imu data
+    // IMU可选
     // Input images: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.
     // Returns the camera pose (empty if tracking fails).
     Sophus::SE3f TrackMonocular(const cv::Mat &im, const double &timestamp, const vector<IMU::Point>& vImuMeas = vector<IMU::Point>(), string filename="");
 
 
     // This stops local mapping thread (map building) and performs only camera tracking.
+    // 激活定位模式，相当于停止LocalMapping线程。
     void ActivateLocalizationMode();
     // This resumes local mapping thread and performs SLAM again.
+    // 恢复定位+建图模式。
     void DeactivateLocalizationMode();
 
     // Returns true if there have been a big map change (loop closure, global BA)
     // since last call to this function
+    // 如果地图有大变化返回true，因此最后应调用一次查询是否要重绘地图。
     bool MapChanged();
 
     // Reset the system (clear Atlas or the active map)
@@ -137,6 +144,7 @@ public:
     // All threads will be requested to finish.
     // It waits until all threads have finished.
     // This function must be called before saving the trajectory.
+    // 保存轨迹前调用以关闭所有线程。
     void Shutdown();
     bool isShutDown();
 
@@ -173,6 +181,7 @@ public:
 
     // Information from most recent processed frame
     // You can call this right after TrackMonocular (or stereo or RGBD)
+    // 获取最新的跟踪状态，一般在Track*()之后调用。
     int GetTrackingState();
     std::vector<MapPoint*> GetTrackedMapPoints();
     std::vector<cv::KeyPoint> GetTrackedKeyPointsUn();
@@ -197,34 +206,42 @@ private:
     void SaveAtlas(int type);
     bool LoadAtlas(int type);
 
-    string CalculateCheckSum(string filename, int type);
+    string CalculateCheckSum(string filename, int type); // 计算MD5
 
     // Input sensor
+    // 传感器类型
     eSensor mSensor;
 
     // ORB vocabulary used for place recognition and feature matching.
+    // ORB磁带，作为识别和特征匹配的容器
     ORBVocabulary* mpVocabulary;
 
     // KeyFrame database for place recognition (relocalization and loop detection).
+    // 关键帧数据库，作为位置识别(重定位和回环检测)的容器
     KeyFrameDatabase* mpKeyFrameDatabase;
 
     // Map structure that stores the pointers to all KeyFrames and MapPoints.
     //Map* mpMap;
+    // 地图集，存放所有关键帧和地图点的指针
     Atlas* mpAtlas;
 
     // Tracker. It receives a frame and computes the associated camera pose.
     // It also decides when to insert a new keyframe, create some new MapPoints and
     // performs relocalization if tracking fails.
+    // Tracking实例，输入帧输出相机位姿，还有选择关键帧，生成地图点，重定位的功能
     Tracking* mpTracker;
 
     // Local Mapper. It manages the local map and performs local bundle adjustment.
+    // LocalMapping实例，管理局部地图和局部BA
     LocalMapping* mpLocalMapper;
 
     // Loop Closer. It searches loops with every new keyframe. If there is a loop it performs
     // a pose graph optimization and full bundle adjustment (in a new thread) afterwards.
+    // LoopClosing实例，每一个关键帧搜索是否回环，若有回环进行位姿图优化和全局BA(全局BA单开线程)
     LoopClosing* mpLoopCloser;
 
     // The viewer draws the map and the current camera pose. It uses Pangolin.
+    // 可视化实例，使用Pangolin绘制地图和相机当前位姿
     Viewer* mpViewer;
 
     FrameDrawer* mpFrameDrawer;
@@ -232,6 +249,7 @@ private:
 
     // System threads: Local Mapping, Loop Closing, Viewer.
     // The Tracking thread "lives" in the main execution thread that creates the System object.
+    // SLAM构造时发射的三个线程，Tracking线程嵌在主线程中
     std::thread* mptLocalMapping;
     std::thread* mptLoopClosing;
     std::thread* mptViewer;
@@ -250,17 +268,25 @@ private:
     bool mbShutDown;
 
     // Tracking state
+    // 7种跟踪状态
+    // SYSTEM_NOT_READY=-1,
+    // NO_IMAGES_YET=0,
+    // NOT_INITIALIZED=1,
+    // OK=2,
+    // RECENTLY_LOST=3,
+    // LOST=4,
+    // OK_KLT=5
     int mTrackingState;
     std::vector<MapPoint*> mTrackedMapPoints;
     std::vector<cv::KeyPoint> mTrackedKeyPointsUn;
     std::mutex mMutexState;
 
-    //
+    // 加载和保存Atlas的文件路径
     string mStrLoadAtlasFromFile;
     string mStrSaveAtlasToFile;
-
+    // 磁带文件路径
     string mStrVocabularyFilePath;
-
+    // 配置文件实例
     Settings* settings_;
 };
 
